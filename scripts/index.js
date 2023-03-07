e6b0d885b5fd5b686f36c6b242e9dc7e04d2d882
AnnLP2 <ann.vitalina@gmail.com>
2023-03-07 15:30:42 +0200

    hw_2 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
 'use strict';
+const a = Number(prompt ("First number"));
+const b = Number(prompt ("Second number"));
+function add (a, b) {
+    console.log(`Result: ${a} + ${b} = ${a + b}`);
+}
+function subtract (a, b) {
+    console.log(`Result: ${a} - ${b} = ${a - b}`);
+}
+function multiply (a, b) {
+    console.log(`Result: ${a} * ${b} = ${a * b}`);
+}
+function divide (a, b) {
+    console.log(`Result: ${a} / ${b} = ${a / b}`);
+}
+add(a, b);
+subtract(a, b);
+multiply(a, b);
+divide(a, b);
