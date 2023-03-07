36c753294520b26f3de39831b579b109f7a8243d
AnnLP2 <ann.vitalina@gmail.com>
2023-03-07 15:42:09 +0200

    hw_4 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
 'use strict';
+const operation = prompt("What do you want to do: + (add), - (sub), * (mult) or / (div)?");
+const firstNumber = Number(prompt("Please, write first number"));
+const secondNumber = Number(prompt("Please, write second number"));
+let result;
+
+if (operation === '+' || operation === 'add') {
+    result = firstNumber + secondNumber;
+}
+else if (operation === '-' || operation === 'sub') {
+    result = firstNumber - secondNumber;
+}
+else if (operation === '*' || operation === 'mult') {
+    result = firstNumber * secondNumber;
+}
+else if (operation=== '/' || operation === 'div') {
+    result = firstNumber / secondNumber;
+}
+else {
+    alert("Incorrect operation!");
+}
+
+if (!isNaN(result)) {
+    alert(`Result: ${firstNumber} ${operation} ${secondNumber} = ${result}`);
+} else {
+    alert("I can't count it =(");
+}
