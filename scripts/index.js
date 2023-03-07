1334d73eeff85dfcfd9e0ed97e62c0012b85d1fe
AnnLP2 <ann.vitalina@gmail.com>
2023-03-07 16:03:46 +0200

    hw_5 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+//Task #1
+const user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+
+//Task #2
+const user = {
+    name: "John",
+}; // это будет работать?
+
+user.name = "Pete";
+alert(user.name); //"Pete"
+
+//Task #3
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+};
+// let sum = salaries.John + salaries.Ann + salaries.Pete;
+// alert(sum);
+let sum = null;
+for (let key in salaries) {
+    sum +=salaries[key];
+}
+alert(sum);
