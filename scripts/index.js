54c1017339fff9e5908ab8e3b7167b34dc947445
AnnLP2 <ann.vitalina@gmail.com>
2023-03-07 15:37:29 +0200

    hw_3 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// Task #1
+let admin;
+const name = "John";
+admin = name;
+alert(admin);
+
+// Task #2
+const ourPlanetName = "Earth";
+const currentUserName = "Ann";
+
+
+// Task #3
+const a = '10' + 10;
+console.log(typeof a); //string
