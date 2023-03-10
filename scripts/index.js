d881330a0b99be43e9e9b1fa2f22b7ee828e1fa0
AnnLP2 <ann.vitalina@gmail.com>
2023-03-10 16:48:04 +0200

    hw_6 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// Task #1
+console.log(Boolean(Number('10')) + 1); // true + 1 => 1 + 1 = 2
+console.log('sub ' + Number(false)); // sub 0
+console.log(16  *  '      91    '); // 1456
+console.log(true - 70); // -69
+console.log(Number(1 + String(1)) + 1); // 12
+
+// Task #2
+
+const hours = +prompt("Please, enter the number of hours");
+const secInHours = hours * 60 * 60;
+if (!isNaN(hours)) {
+    alert(`In ${hours} hours there are ${secInHours} seconds.`);
+} else {
+    alert(`Please, write just a valid number.`);
+}
+
+// Task #3
+
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num ++;
+num --;
+alert(num); // -0.7142857142857143
