1803ef2094a6a6678204777c4f8a44b744bed8c8
AnnLP2 <ann.vitalina@gmail.com>
2023-03-14 01:13:10 +0200

    hw_8, part 2resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let numOrStr = prompt('Input number or string');
+console.log(numOrStr);
+
+switch(true) {
+    case numOrStr === null:
+        console.log('You canceled!');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case isNaN(+numOrStr):
+        console.log('Number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+}
