03c2aee4a5c37b9e86fa8489075080d71bf66a13
AnnLP2 <ann.vitalina@gmail.com>
2023-03-18 16:25:54 +0200

    hw_12 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const x = 4;
+const y = 9;
+const R = 10;
+const z = Math.sqrt((x ** 2) + (y ** 2));
+
+const result = (z <= R) ? 'Точка в окружности' : 'Точка вне окружности';
+console.log(result);
+
