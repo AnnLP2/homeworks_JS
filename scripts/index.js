35346aae59023989e872624e9b97d47f20cbc9ce
AnnLP2 <ann.vitalina@gmail.com>
2023-03-22 01:06:01 +0200

    hw_18 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+function getPositiveNumbers(numbers) {
+    if (numbers.length === 0) {
+        return 'The array is empty!';
+    }
+    const positiveNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > 0) {
+            positiveNumbers.push(numbers[i]);
+        }
+    }
+    return positiveNumbers.length > 0 ? positiveNumbers : null;
+}
+console.log(getPositiveNumbers(arr));
