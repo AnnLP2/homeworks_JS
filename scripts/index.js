27dc3217a1654958635abb655483e0ff6c59957b
AnnLP2 <ann.vitalina@gmail.com>
2023-03-21 23:19:53 +0200

    hw_16 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const num = +prompt('Enter the number');
+const exponent = +prompt('Enter the pow exponent');
+function pow (num, exponent = 1) {
+    return !isNaN(num) && !isNaN(exponent) ? Math.pow(num, exponent) : 'Some error!';
+}
+const result = pow(num, exponent);
+alert(result);
