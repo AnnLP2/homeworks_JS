eeda9e5839d0e544a331cc9183bb7855744f8bdc
AnnLP2 <ann.vitalina@gmail.com>
2023-03-23 17:52:27 +0200

    hw_19 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function totalCostPotatoes(borschLiters) {
+    const onePotatoWeight = 0.075;
+    const oneBorschLiter = onePotatoWeight * 4;
+    const pricePerKgPotato = 13;
+    const totalWeightPotatoes = Math.ceil(oneBorschLiter * borschLiters);
+    return pricePerKgPotato * totalWeightPotatoes;
+}
+
+console.log(totalCostPotatoes(48));
