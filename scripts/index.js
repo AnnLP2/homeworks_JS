518e34f5b5f983a9ad2fda9011fc4f685886906c
AnnLP2 <ann.vitalina@gmail.com>
2023-03-23 16:00:44 +0200

    hw_20 init

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function fib(n = 1) {
+    let fibNumber = 0;
+    const arrOfFibonacciNumbers = [];
+    for (let i = 0; i < arrOfFibonacciNumbers.length; i ++) {
+        if (fibNumber >= 0) {
+            arrOfFibonacciNumbers.push(i);
+        }
+    }
+    console.log(arrOfFibonacciNumbers);
+
+}
+
