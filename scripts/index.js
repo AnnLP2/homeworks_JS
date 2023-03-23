7f120b52ba5f586471f2e02728ebb94c3a176a66
AnnLP2 <ann.vitalina@gmail.com>
2023-03-23 16:03:00 +0200

    hw_18 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,13 @@
 const arr = [1, 2, 3, -1, -2, -3];
 
 function getPositiveNumbers(numbers) {
-    if (numbers.length === 0) {
-        return 'The array is empty!';
-    }
+    if (numbers.length === 0) return 'The array is empty!';
+
     const positiveNumbers = [];
     for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > 0) {
-            positiveNumbers.push(numbers[i]);
-        }
+        if (numbers[i] > 0) positiveNumbers.push(numbers[i]);
     }
     return positiveNumbers.length > 0 ? positiveNumbers : null;
 }
+
 console.log(getPositiveNumbers(arr));
