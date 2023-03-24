6a8628963fdab5944189e28dfccad0f6de3380a2
AnnLP2 <ann.vitalina@gmail.com>
2023-03-24 14:46:44 +0200

    hw_21 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const num = +prompt('Введите число');
+if (Number.isNaN(num)) {
+    alert('Invalid number');
+}
+let isPrime = true;
+for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+        isPrime = false;
+        break;
+    }
+}
+
+console.log(isPrime);
