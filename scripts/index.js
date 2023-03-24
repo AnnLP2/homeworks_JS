0995d67e0cb99923be6b936cdfceea74e66a4262
AnnLP2 <ann.vitalina@gmail.com>
2023-03-25 00:26:57 +0200

    hw_22 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const num = +prompt("enter number");
+let minDivisor = num;
+if (num < 1) {
+    minDivisor = NaN;
+}
+for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+        minDivisor = i;
+        break;
+    }
+}
+
+console.log(minDivisor);
