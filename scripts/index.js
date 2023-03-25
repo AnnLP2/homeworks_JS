ac117fb033d4b25752d9dec908bb2ec69597bebc
AnnLP2 <ann.vitalina@gmail.com>
2023-03-25 18:33:21 +0200

    hw_22 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,28 @@
 'use strict';
 
-const num = +prompt("Enter the number");
-let minDivisor = num;
-if (num < 1) {
-    minDivisor = NaN;
-}
-for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
-        minDivisor = i;
-        break;
+const num = +prompt('Enter the number');
+function getMinDivisor(num) {
+    if(num < 1) return NaN;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) return i;
     }
+    return num;
 }
 
-console.log(minDivisor);
+console.log(getMinDivisor(num));
+
+
+// const num = +prompt("Enter the number");
+// let minDivisor = num;
+// if (num < 1) {
+//     minDivisor = NaN;
+// }
+// for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//         minDivisor = i;
+//         break;
+//     }
+// }
+//
+// console.log(minDivisor);
+
