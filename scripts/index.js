f9ac3fc54a3b1ffb7f6bb09e8558a5376dd031e1
AnnLP2 <ann.vitalina@gmail.com>
2023-04-21 22:45:59 +0300

    hw_30 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,26 @@
-'use strict';
+"use strict";
+
+// Task #1
+const getFactorial = (n) => {
+  return n === 0 ? 1 : n * getFactorial(n - 1);
+};
+
+console.log(getFactorial(3));
+
+// Task #2
+const pow = (num, degree) => {
+  return degree === 1 ? num : num * pow(num, degree - 1);
+};
+
+console.log(pow(3, 4));
+
+
+// Task #3
+const sum = (a, b) => {
+  if (b === 0) return a;
+  return sum(a + 1, b - 1);
+};
+
+console.log(sum(10, 7));
+
+
