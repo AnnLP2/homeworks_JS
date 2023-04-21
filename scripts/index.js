b41d7a4fb9aecca17a719c6140c11870df96adee
AnnLP2 <ann.vitalina@gmail.com>
2023-04-22 00:56:43 +0300

    hw_33 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
-'use strict';
+"use strict";
+
+const sum = (function() {
+  let num = 0;
+
+  return function fn(n) {
+    num = num + n;
+    return num;
+  };
+})();
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
+
