74bbf464629ffdcf8f0e8fbeac9fe4c92b7b141c
AnnLP2 <ann.vitalina@gmail.com>
2023-04-24 10:42:52 +0300

    hw_34 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,28 @@
-'use strict';
+"use strict";
+
+const getRandom = (function() {
+  const usedNums = [];
+
+  const genRandomNum = () => Math.floor(Math.random() * 100) + 1;
+  return function() {
+    let randomNum = genRandomNum();
+    if (usedNums.length >= 100) return NaN;
+    while (usedNums.includes(randomNum)) {
+      randomNum = genRandomNum();
+    }
+    usedNums.push(randomNum);
+    return randomNum;
+  };
+})();
+
+console.log(getRandom());
+console.log(getRandom());
+console.log(getRandom());
+console.log(getRandom());
+console.log(getRandom());
+
+
+
+
+
 
