0a95b93fb3d0dbddbfa451c969acdcaa425d6bdb
AnnLP2 <ann.vitalina@gmail.com>
2023-04-26 00:45:47 +0300

    hw_36 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,20 @@
-'use strict';
+"use strict";
+
+const calculator = {
+  read(a, b) {
+    if (typeof a !== "number") throw new Error("\"a\" needs to be of type number");
+    if (typeof b !== "number") throw new Error("\"b\" needs to be of type number");
+    this.a = a;
+    this.b = b;
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  }
+};
+
+calculator.read(3, 4);
+alert(calculator.sum());
+alert(calculator.mul());
