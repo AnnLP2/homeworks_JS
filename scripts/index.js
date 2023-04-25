f6559cab9b4a2e3b734b988ee1acdd2b91afbdb5
AnnLP2 <ann.vitalina@gmail.com>
2023-04-25 18:59:18 +0300

    hw_32 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
-'use strict';
+"use strict";
+
+void function() {
+  const exampleArr = [1, 2, [3, [4, 5, 6, [7, 8]]]];
+  const flat = (...args) => {
+    if (args.length !== 1) throw new Error("Function accepts only 1 argument, too much arguments provided");
+    const [arr] = args;
+    const resultArr = [];
+
+    const flatter = (arr) => {
+      for (const item of arr) {
+        !Array.isArray(item) ? resultArr.push(item) : flatter(item);
+      }
+    };
+    flatter(arr);
+    return resultArr;
+  };
+
+  console.log(flat(exampleArr));
+}();
+
+
