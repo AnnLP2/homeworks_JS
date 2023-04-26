eb26f063ce7de2c177f6187f03f1ab686884513a
AnnLP2 <ann.vitalina@gmail.com>
2023-04-27 01:04:15 +0300

    hw_35 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,44 @@
 "use strict";
 
 const counter = (num = 0) => {
-  let count = num;
+    let count = num;
+    let statistic = {valueCalls: 0, increaseCalls: 0, decreaseCalls: 0};
+    return {
+        value() {
+            statistic.valueCalls++;
+            return count;
+        },
 
-  const value = () => {
-    return count;
-  }
+        increase() {
+            statistic.increaseCalls++;
+            count++;
+        },
 
-  const increase = () => {
-    count++;
-  };
+        decrease() {
+            statistic.decreaseCalls++;
+            count--;
+        },
 
-  const decrease = () => {
-    count--;
-  }
+        getStatistic() {
+            return statistic;
+        },
+
+        reset() {
+            count = num;
+            Object.keys(statistic).forEach(key => {statistic[key] = 0});
+        }
+
+    }
 };
+
+const counter1 = counter(5);
+counter1.decrease();
+counter1.increase();
+counter1.increase();
+counter1.decrease();
+counter1.decrease();
+console.log(counter1.value());
+console.log(counter1.getStatistic());
+counter1.reset();
+console.log(counter1.getStatistic());
+console.log(counter1.value());
