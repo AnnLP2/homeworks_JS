016edabef4fd6ab48d4425d93a94f550897725fc
AnnLP2 <ann.vitalina@gmail.com>
2023-04-26 23:58:41 +0300

    hw_37 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+
+
+function bind(fn, thisArg, ...bindArgs) {
+
+  return function(...args) {
+    return fn.call(thisArg, ...bindArgs, ...args);
+  };
+}
+
+const foo = function() {
+  console.log(this);
+};
+
+const boundedFoo = bind(foo, [1, 2, 3, 4]);
+boundedFoo();
+
+
