17c135d51ace0e66efb7376fb0c07a78ceadf106
AnnLP2 <ann.vitalina@gmail.com>
2023-05-09 23:37:13 +0300

    hw_43 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const createTable = () => {
+
+    const table = document.createElement('table');
+
+    for (let i = 1; i <= 10; i++) {
+        const row = document.createElement('tr');
+        for (let j = 1; j <= 10; j++) {
+            const cell = document.createElement('td');
+            const num = (i - 1) * 10 + j;
+            cell.textContent = num.toString();
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+    document.body.appendChild(table);
+}
+
+createTable();
+
+
