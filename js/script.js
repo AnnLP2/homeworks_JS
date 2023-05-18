19b8d9d827d4fc9089742f8c16df1ed43f750191
AnnLP2 <ann.vitalina@gmail.com>
2023-05-18 23:37:16 +0300

    hw_44 resolved

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
 'use strict';
 
-const form = document.querySelector('#form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const select = form.elements.option;
-    const termsCheckbox = form.querySelector('input[name="terms"]');
-    console.log({
-        email: formData.get('email'),
-        firstName: formData.get('firstName'),
-        lastName: formData.get('lastName'),
-        nickname: formData.get('nickname'),
-        password: formData.get('password'),
-        file: formData.get('file'),
-        select: `Option ${select.value}`,
-        message: formData.get('message'),
-        isTermsChecked: termsCheckbox.checked,
+(function () {
+    const form = document.querySelector('#form');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        const data = {};
+        form.querySelectorAll('input, textarea, select').forEach(input => {
+            data[input.name] = input.value;
+        });
+        console.log(data);
     });
-});
+})();
+
