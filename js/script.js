f4c6bfdee8391142bab591747ed1ea8124021496
AnnLP2 <ann.vitalina@gmail.com>
2023-05-19 02:05:27 +0300

    hw_44 resolved

--- js/script.js
+++ js/script.js
@@ -7,11 +7,12 @@
         event.preventDefault();
         event.stopPropagation();
         const data = {};
+        const terms = form.querySelector('input.form-check-input');
         form.querySelectorAll('input, textarea, select').forEach(input => {
+            if(!terms.checked) throw new Error("You need to agree to the terms");
             data[input.name] = input.value;
         });
         console.log(data);
-
     });
 })();
 
