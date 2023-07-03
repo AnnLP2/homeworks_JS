a33e630fb7f0c7b879534c3dac36cb54a0abbd81
AnnLP2 <ann.vitalina@gmail.com>
2023-07-03 21:59:13 +0300

    hw_67 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+
+(function() {
+  function getSumOfGoodsPrice(goods) {
+
+    if (!Array.isArray(goods)) throw new Error("It should be an array!");
+
+    const goodsMap = new Map();
+
+    goods.forEach(([good, price]) => {
+      goodsMap.set(good, price);
+    });
+
+    let sum = 0;
+
+    for (const price of goodsMap.values()) {
+      sum += price;
+    }
+
+    return sum;
+  }
+
+  const goods = [["футболка", 15], ["шорты", 25], ["носки", 5]];
+  const goods1 = [["платье", 30], ["джинсы", 50], ["штаны", 10]];
+  console.log(getSumOfGoodsPrice(goods));
+  console.log(getSumOfGoodsPrice(goods1));
+
+})();
