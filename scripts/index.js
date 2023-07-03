7f532f4d5553878648c3e7a4bf4eae2455050a00
AnnLP2 <ann.vitalina@gmail.com>
2023-07-04 00:55:25 +0300

    hw_71 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+
+(function() {
+  function printGreetings(students) {
+    for (const { name, subject } of students) {
+      console.log(`Привет, ${name}! Ты изучаешь предмет ${subject}`);
+    }
+  }
+
+  const students = [
+    { name: "John", age: 20, subject: "Math" },
+    { name: "Alice", age: 22, subject: "English" },
+    { name: "Bob", age: 21, subject: "History" }
+  ];
+
+  printGreetings(students);
+
+})();
