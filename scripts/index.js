fee2f676ecbbf50eb0ee9a7c977a28f49a5fd43d
AnnLP2 <ann.vitalina@gmail.com>
2023-07-04 01:04:02 +0300

    hw_70 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,19 @@
 "use strict";
 
-function addPlayerToLevelSet(...players) {
-  const playersWeakSet = new WeakSet();
-  for (const player of players) {
-    playersWeakSet.add(player);
+(function() {
+  function addPlayerToLevelSet(...players) {
+    const playersWeakSet = new WeakSet();
+    for (const player of players) {
+      playersWeakSet.add(player);
+    }
+    return playersWeakSet;
   }
-  return playersWeakSet;
-}
 
-const player1 = { id: 1, name: "Иван", level: 10 };
-const player2 = { id: 2, name: "Иван", level: 10 };
-const player3 = { id: 3, name: "Иван", level: 10 };
-const player4 = { id: 4, name: "Иван", level: 10 };
-const player5 = { id: 5, name: "Иван", level: 10 };
+  const player1 = { id: 1, name: "Иван", level: 10 };
+  const player2 = { id: 2, name: "Иван", level: 10 };
+  const player3 = { id: 3, name: "Иван", level: 10 };
+  const player4 = { id: 4, name: "Иван", level: 10 };
+  const player5 = { id: 5, name: "Иван", level: 10 };
 
-
-console.log(addPlayerToLevelSet(player1, player2, player3, player4, player5));
+  console.log(addPlayerToLevelSet(player1, player2, player3, player4, player5));
+})();
