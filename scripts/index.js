1d875b8c18649558aacfb0dc617df701e87e5ed3
AnnLP2 <ann.vitalina@gmail.com>
2023-07-04 00:11:33 +0300

    hw_69 resolved

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-'use strict';
+"use strict";
+
+(function() {
+  function saveGrades(student, grades) {
+    const studentWeakMap = new WeakMap();
+    return studentWeakMap.set(student, grades);
+  }
+
+  const student = { name: "Иван Иванов" };
+  const grades = { math: 90, fiz: 85, eng: 95 };
+
+  console.log(saveGrades(student, grades));
+
+})();
