b16cfaea79a33691e565f5f9711ae9b5a2f5fcad
AnnLP2 <ann.vitalina@gmail.com>
2023-08-23 17:48:57 +0300

    hw_78 resolved

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card title='hi'/>
+      <Card text='how are you?'/>
+      <Card title='hi' text='how are you?'/>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 class Card extends React.Component {
   render() {
+    const { title, text } = this.props;
     return <div className="card">
       <div className="card-body">
-        <h4 className="card-title">Card title</h4>
-        <p className="card-text">Some quick example text to build on the card</p>
-        <button type="button" className="btn btn-primary">Go somewhere</button>
+        {title && <h4 className="card-title">{title}</h4>}
+        {text && <p className="card-text">{text}</p>}
       </div>
     </div>;
   }
 }
 
+Card.defaultProps = {
+  title: null,
+  text: null
+};
 export default Card;
